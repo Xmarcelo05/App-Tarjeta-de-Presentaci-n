bfed463570814882bae4dcc3c748dae0313b47b0
Xmarcelo-05 <fremarbrapo@gmail.com>
2026-09-13 00:59:44 -0500

    Boxes y Columns definidos para la estructura básica de la app.

--- app/src/main/java/com/example/tarjetapresentacion/MainActivity.kt
+++ app/src/main/java/com/example/tarjetapresentacion/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tarjetapresentacion.ui.theme.TarjetaPresentacionTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +29,66 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TarjetaPresentacionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            TarjetaPresentacionTheme{
+                TarjetaPresentacion()
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TarjetaPresentacion() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF9CE9DE)),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(top = 150.dp)
+                .padding(bottom = 20.dp),
+            verticalArrangement = Arrangement.spacedBy(100.dp)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TarjetaPresentacionTheme {
-        Greeting("Android")
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(200.dp)
+                    .width(300.dp)
+                    .background(Color(0xFF023222))
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.android_logo),
+                        contentDescription = "Logo de Android",
+                        modifier = Modifier.size(150.dp)
+                    )
+                    Text(text = "Hello World")
+                }
+
+            }
+
+            Box(
+                modifier = Modifier
+                    .height(300.dp)
+                    .width(300.dp)
+                    .background(Color(0xFF021119))
+            ){
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ){
+
+                }
+
+
+            }
+        }
     }
 }
